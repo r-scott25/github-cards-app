04f30832e706d64ec1ec4fff53dac0e52076686b
r-scott25 <rachelbernal2@gmail.com>
2022-12-22 12:22:59 -0600

    initializing and reading the State Object

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Form.css';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form actoin="">
+        <input type="text" placeholder="GitHub username" />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
+
+export default Form;

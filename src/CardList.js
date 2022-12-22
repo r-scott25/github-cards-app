519af4d3a74c3ba4cae027ace0c8dcc26bb19f03
r-scott25 <rachelbernal2@gmail.com>
2022-12-22 13:46:01 -0600

    test

--- src/CardList.js
+++ src/CardList.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import Card from './Card';
-import './CardList.css';
-
+import React from "react";
+import Card from "./Card";
+import "./CardList.css";
 
 const CardList = (props) => (
   <div>
-    {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+    {props.profiles.map((profile) => (
+      <Card key={profile.id} {...profile} />
+    ))}
   </div>
 );
 
 export default CardList;
+
+
+// test
